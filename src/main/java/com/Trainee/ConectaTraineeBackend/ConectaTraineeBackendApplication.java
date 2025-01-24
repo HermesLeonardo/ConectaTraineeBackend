5429348dde336c97874bcbbeba299b4c117cdc79
Leonardo Hermes <leohermescarvalho18@gmail.com>
2025-01-24 11:38:23 -0300

    Criação do repositorio

---
+++ src/main/java/com/Trainee/ConectaTraineeBackend/ConectaTraineeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Trainee.ConectaTraineeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaTraineeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaTraineeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Trainee/ConectaTraineeBackend/ConectaTraineeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Trainee.ConectaTraineeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaTraineeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
